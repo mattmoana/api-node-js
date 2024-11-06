93e6aa9800a3dfbcc93a5d8f75f5e0d99a708d81
mattmoana <matt.morozov@oc.eu>
2024-11-06 16:31:22 +0200

    added initial api tests

--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -12,23 +12,46 @@ test.describe('User management API', () => {
     });
 
     test('GET /:id - should return a user by ID', async ({ request }) => {
+        const response = await request.post(`${baseURL}`);
+        expect(response.status()).toBe(201);
+        const responseBody = await response.json()
+        await expect.soft(responseBody.id).toBeDefined()
+        const id = responseBody.id
+        console.log(id)
+
+        const response2 = await request.get(`${baseURL}/${id}`);
+        const responseBody2 = await response2.json()
+        await expect.soft(responseBody2.id).toBe(id)
+
 
     });
 
     test('GET /:id - should return 404 if user not found', async ({ request }) => {
-
+        const response = await request.post(`${baseURL}/1000`);
+        expect(response.status()).toBe(404);
     });
 
     test('POST / - should add a new user', async ({ request }) => {
-
+        const response = await request.post(`${baseURL}`);
+        expect(response.status()).toBe(201);
+        const responseBody = await response.json()
+        await expect.soft(responseBody.id).toBeDefined()
     });
 
     test('DELETE /:id - should delete a user by ID', async ({ request }) => {
+        const response = await request.post(`${baseURL}`);
+        expect(response.status()).toBe(201);
+        const responseBody = await response.json()
+        const id = responseBody.id
+
+        const response2 = await request.delete(`${baseURL}/${id}`);
+        await expect(response2.status()).toBe(200)
 
     });
 
     test('DELETE /:id - should return 404 if user not found', async ({ request }) => {
-
+        const response = await request.delete(`${baseURL}/1000`);
+        expect(response.status()).toBe(404);
     });
 
 
